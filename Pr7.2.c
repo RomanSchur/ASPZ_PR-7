c72e3f013202437091fa32f06557066c0a1a768c
RomanSchur <161126957+RomanSchur@users.noreply.github.com>
2025-05-07 20:27:12 +0300

    Add files via upload

---
+++ Pr7.2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+void print_permissions(mode_t mode) {
+    printf((S_ISDIR(mode)) ? "d" : "-");
+    printf((mode & S_IRUSR) ? "r" : "-");
+    printf((mode & S_IWUSR) ? "w" : "-");
+    printf((mode & S_IXUSR) ? "x" : "-");
+    printf((mode & S_IRGRP) ? "r" : "-");
+    printf((mode & S_IWGRP) ? "w" : "-");
+    printf((mode & S_IXGRP) ? "x" : "-");
+    printf((mode & S_IROTH) ? "r" : "-");
+    printf((mode & S_IWOTH) ? "w" : "-");
+    printf((mode & S_IXOTH) ? "x" : "-");
+    printf(" ");
+}
+
+int main() {
+    DIR *dir;
+    struct dirent *entry;
+    struct stat file_stat;
+
+    dir = opendir(".");
+
+    if (dir == NULL) {
+         return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (stat(entry->d_name, &file_stat) == -1) {
+            continue;
+        }
+        print_permissions(file_stat.st_mode);
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+    return 0;
+}
